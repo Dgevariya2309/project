c9f9f9987b845c70f9bff40f96eccd30938e626a
Astha <asthavekariya0810@gmail.com>
2024-07-02 21:22:49 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,66 @@
+<?php
+include 'dbcon.php';
+include 'header.php';
+?>
+
+<html>
+
+<head lang="en">
+<title>Hotel Management</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <div class=" box-shadow-all" id="booking-hall">
+        <img class="img-fluid hall-img" src="img/dinning1.jpg" alt="dinning">
+        <div class="top-content hall-div">
+            <!-- <div class="h1">
+                    <h1 class="about-h1">Welcome To Our Website</h1>
+                </div> -->
+            
+
+        </div>
+    </div>
+
+    <div style="font-size:16px; color:black; font-family: 'Baloo 2', cursive;">
+    <br>
+    <p class="about-p">-->Room service or in-room dining is a hotel service enabling guests to choose items of
+                food and drink for delivery to their hotel room for consumption. Room service is organised as a
+                subdivision within the food and beverage department of high-end hotel and resort properties.
+                It is uncommon for room service to be offered in hotels that are not high-end, or in motels.</p>
+    <br>
+    <p class="about-p">-->Room service may also be provided for guests on cruise ships. Room service may be provided on a 
+                24-hour basis or limited to late night hours only. Due to the cost of customized orders and 
+                delivery of room service, prices charged to the patron are typically much higher than in the
+                hotel's restaurant or tuck shop, and a gratuity is expected.</p>
+    <br>
+    <p class="about-p">-->Room service or in-room dining is a hotel service enabling guests to choose 
+                items of food and drink for delivery to their hotel room for consumption. Room service is 
+                organised as a subdivision within the food and beverage department of high-end hotel and 
+                resort properties. It is uncommon for room service to be offered in hotels.</p>
+    </div>
+
+</body>
+
+</html>
+
+<?php
+include 'footer.php';?>
+
+<!-- TODO -->
