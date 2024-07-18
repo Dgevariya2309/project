c9efa818162d8ed5b443196c5c5f3f4ea81dcf31
Astha <asthavekariya0810@gmail.com>
2024-07-18 22:55:45 +0530

    Update homerooms.php

--- homerooms.php
+++ homerooms.php
@@ -1,1 +1,51 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
 
+    <section class="card box-shadow-all" id="rooms">
+        <div class="card-body" id="rooms-content-right">
+            <div class="paras box-shadow-all">
+                <p class="card-title sectionTag">A.C Delux Room</p>
+                <p class="sectionsubTag font">We have the best services provider in Room Management. with 24 hours room
+                    services and 24 hours check-out. We provide first class services in very low cost.</p>
+                <a href="index.php"><button class="price-btn">Book A Room</button></a>
+            </div>
+            <div class="box-shadow-all">
+                <img class="rooms-img" src="img/rooms1.jpg" alt="delux" />
+            </div>
+        </div>
+    </section>
+
+    <section class="card box-shadow-all" id="rooms">
+        <div class="card-body" id="rooms-content-left">
+        <div class="box-shadow-all">
+                <img class="rooms-img" src="img/hal4.jpg" alt="delux" />
+            </div>
+            <div class="paras box-shadow-all">
+                <p class="card-title sectionTag">Hall</p>
+                <p class="sectionsubTag font">We have the best services provider in Hall Management. with 24 hours room
+                    services. We provide first class services in very low cost.</p>
+                <a href="index.php"><button class="price-btn">Book A Hall</button></a>
+            </div>
+        </div>
+    </section>
+
+    <section class="card box-shadow-all" id="rooms">
+        <div class="card-body" id="rooms-content-right">
+            <div class="paras box-shadow-all">
+                <p class="card-title sectionTag">Food</p>
+                <p class="sectionsubTag font">We have the best services provider in Food Management. with 24 hours room
+                    services. We provide first class services in very low cost.</p>
+                <a href="index.php"><button class="price-btn">Book A Food</button></a>
+            </div>
+            <div class="box-shadow-all">
+                <img class="rooms-img" src="img/f1.webp" alt="delux" />
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
